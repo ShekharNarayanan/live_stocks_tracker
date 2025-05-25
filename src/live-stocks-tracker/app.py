ee88bbb6b79849fb04b827beec8e62ce3d357c6b
ShekharNarayanan <shekharnarayanan833@gmail.com>
2025-05-25 12:10:21 +0200

    Make ticker info bigger and more noticeable. Pt 1,2 addressed. #11

--- src/live-stocks-tracker/app.py
+++ src/live-stocks-tracker/app.py
@@ -58,6 +58,21 @@ def rsi(series, n=14):
     rs    = gain / loss.replace(0, np.nan)
     return 100 - 100 / (1 + rs)
 
+def get_logo_from_symbol(symbol, size=30):
+    """
+    Get the logo URL for a given stock symbol from Parqet. Check it out here:
+    https://www.parqet.com/api/logos
+
+    Args:
+        symbol (str): Stock symbol to get the logo for, e.g. 'AAPL' for Apple Inc.
+        size (int, optional): Size of the logo in pixels. Defaults to 30 x30.
+
+    Returns:
+        _type_: _description_
+    """
+    img_url = f"https://assets.parqet.com/logos/symbol/{symbol}?format=jpg&size={size}"
+    
+    return img_url
 def render_blocks(df):
     cols = st.columns(4)
     for idx, row in df.reset_index(drop=True).iterrows():
@@ -66,20 +81,20 @@ def render_blocks(df):
             st.markdown(
                 f"""
                 <div style="
-                     border:6px solid #004080;
-                     border-radius:6px;
-                     padding:10px 12px;
-                     margin-bottom:12px;
-                     ">
+                    border:6px solid #004080;
+                    border-radius:6px;
+                    padding:5px 12px;
+                    margin-bottom:12px;
+                    ">
                     <h3 style="margin:0">{row.Symbol}</h3>
-                    <small style="color:#aaaaaa;">{row.Sector if row.Sector!='-' else ''}</small><br>
-                    <span style="font-size:18px; color:{'red' if row.Change<0 else 'green'};">
+                    <span style="font-size:25px;color:#aaaaaa;">{row.Sector if row.Sector!='-' else ''}</span><br>
+                    <span style="font-size:25px; color:{'red' if row.Change<0 else 'green'};">
                         {'↓' if row.Change<0 else '↑'} {row.Change:+.2f}%
                     </span><br>
-                    <span style="color:green;">Now €{row.Today:.2f}</span><br>
-                    <span style="color:red;">{days} d ago €{row.Ago:.2f}</span><br>
-                    RSI-14: <b>{row.RSI:.1f}</b><br>
-                    Avg Vol (30 d): {row.AvgVol:,.0f}
+                    <span style="font-size:25px;color:green;">Now €{row.Today:.2f}</span><br>
+                    <span style="font-size:25px;color:red;">{days} d ago €{row.Ago:.2f}</span><br>
+                    <span style="font-size:20px;">RSI-14: {row.RSI:.1f}</span><br>
+                    <span style="font-size:20px;">Avg Vol (30 d): {row.AvgVol:,.0f}</span>
                 </div>
                 """,
                 unsafe_allow_html=True
