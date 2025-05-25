0eaf8260b08aa922b527a2af38e290b834153129
ShekharNarayanan <shekharnarayanan833@gmail.com>
2025-05-25 11:22:00 +0200

    Correct folder name and change bat script to accomodate

--- run_tracker.bat
+++ run_tracker.bat
@@ -1,13 +1,13 @@
 @echo off
 REM 1) go to your app folder
-cd  "C:\Users\narayana\projects\personal\live-stocks-tracker\src\live-stocks-tracker"
+cd  "C:\Users\narayana\projects\personal\live-stocks-tracker\src"
+dir
 
 REM 2) activate conda
 call conda activate live-tracker
-echo "conda env activated"
 
 REM 3) run the streamlit app
-call streamlit run app.py
+call streamlit run live-stocks-tracker\app.py
 pause
 
 
