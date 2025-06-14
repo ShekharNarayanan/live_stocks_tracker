135a07c0a01380ab1c9f57b822131811e79d1130
ShekharNarayanan <shekharnarayanan833@gmail.com>
2025-06-14 21:12:52 +0200

    Update .bat script to match project structure

--- run_tracker.bat
+++ run_tracker.bat
@@ -1,12 +1,12 @@
 @echo off
 REM 1) go to your app folder
-cd  "C:\Users\narayana\projects\personal\live-stocks-tracker\src"
+cd  "C:\Users\narayana\projects\personal\live_stocks_tracker\src"
 
 REM 2) activate conda
 call conda activate live-tracker
 
 REM 3) run the streamlit app
-call streamlit run live-stocks-tracker\app.py
+call streamlit run live_stocks_tracker\app.py
 pause
 
 
