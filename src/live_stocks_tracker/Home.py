40ca12c71038db95bd33f0716eef08ea01037cba
ShekharNarayanan <shekharnarayanan833@gmail.com>
2025-08-06 12:17:50 +0200

    Add info on both pages and speed up animation

--- src/live_stocks_tracker/Home.py
+++ src/live_stocks_tracker/Home.py
@@ -2,6 +2,8 @@
 import streamlit as st
 from utilities.animations import add_half_screen_stock_glow
 
+add_half_screen_stock_glow() 
+
 st.set_page_config(
     page_title="Live-Stocks-Tracker",
     page_icon="💹",
@@ -14,8 +16,10 @@ st.set_page_config(
 st.markdown(
     """
     <h1 style='font-size:3rem; margin-bottom:0;'>Live-Stocks-Tracker</h1>
-    <p style='font-size:1.3rem; color:#8c8c8c; margin-top:0.2rem;'>
+    <p style='font-size:1.3rem; color:#8c8c8c; margin-top:0.2rem; font-style:italic;'>
         Welcome to Live-Stocks-Tracker! A simple add-free web application that helps you keep track of stocks in the US market using yfinance.
+
+        P.S: Apologies for the load times! 🙏 yfinance is free but slow 
     </p>
     """,
     unsafe_allow_html=True,
@@ -27,20 +31,28 @@ col1, col2 = st.columns([1, 3])
 with col1:
     if st.button("🚀 Open the generalized Scanner", use_container_width=True):
         st.switch_page("pages/Scanner.py")
-    if st.button("📊 Open the Portfolio Tracker", use_container_width=True):
-        st.switch_page("pages/Portfolio.py")
+    st.info(
+    """
+    **Scanner** \n
+    Find out the top-20 gainers and losers in the S&P 500/400/600. 
+    
+    """,
+    icon="ℹ️",
+    )
+    
 
 with col2:
+    if st.button("📊 Open the Portfolio Tracker", use_container_width=False):
+        st.switch_page("pages/Portfolio.py")
     st.info(
-        """
-        **What do the different options mean?**
+    """
+    **Watchlist** \n
+    Track companies of your choice in either of the three S&P universes! You can choose to just explore or sign up so your watchlist is saved for you 🙌
+    """,
+    icon="ℹ️",
 
-        * 🌐 We pull daily prices for the S&P 500/400/600 via *yfinance*  
-        * 📊 Compute %-change, RSI-14, and average volume  
-        * 📈 Display top-20 gainers and losers in each cap-size category
-        """,
-        icon="ℹ️",
     )
+    
 
 # Hide sidebar on the landing page so the focus stays on the hero section
 st.markdown(
@@ -51,4 +63,3 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
-add_half_screen_stock_glow() 
