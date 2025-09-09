4746d50d3d46ec285aeb733dd23c8d177e012ced
ShekharNarayanan <shekharnarayanan833@gmail.com>
2025-09-09 15:15:43 +0200

    Update bat script after recreating env

--- run_tracker.bat
+++ run_tracker.bat
@@ -3,7 +3,7 @@ REM 1) go to your app folder
 cd  "C:\Users\narayana\projects\personal\live_stocks_tracker\src"
 
 REM 2) activate conda
-call conda activate live-tracker
+call conda activate live_stocks_tracker
 
 REM 3) run the streamlit app
 call streamlit run live_stocks_tracker\Home.py
