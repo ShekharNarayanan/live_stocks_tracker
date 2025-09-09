546776f5ec966d12f041b793d31dfb453b2585f6
ShekharNarayanan <shekharnarayanan833@gmail.com>
2025-09-09 15:20:15 +0200

    Add a separate personal watchlist page #13

---
+++ src/live_stocks_tracker/pages/Your_Portfolio.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from pathlib import Path
+from utilities.db_utils import  fetch_portfolio_from_db
+from utilities.adjust_ui import render_company_blocks
+
+
+#  ── PATHS ────────────────────────────────────────────────────────────────
+parent_dir = Path(__file__).parents[3]
+DB_PATH = parent_dir / "portfolios.db"
+
+#   ── DISPLAY PORTFOLIO ────────────────────────────────────────────────────────────────
+if "user_email" in st.session_state:
+    st.write(f"Welcome back, {st.session_state.user_email}!\n Here is your portfolio")
+
+    # ── ADD RADIO SETTINGS ──────────────────────────────────────────────────────────────────
+    cap_size = st.radio(
+        "Cap Size universe",
+        [
+            "Large (S&P 500)",
+            "Mid (S&P 400)",
+            "Small (S&P 600)",
+        ],  # categories on the side bar
+        horizontal=True,
+    )
+
+    
+    symbols, portfolio_df = fetch_portfolio_from_db(
+    st.session_state.user_email,
+    cap_size,  # default cap size for now
+    DB_PATH=DB_PATH
+)
+    if symbols:
+        render_company_blocks(ticker_stats_df=portfolio_df)
+    else:
+        st.warning(f"Whoops! You dont have any tickers from the {cap_size} universe.")
+        if st.button("Add new tickers ➕"):
+            st.switch_page("pages/Make_Your_Portfolio.py")
+
+else:
+    st.warning("Please log in on the watchlist page to access your saved portfolio")
+    if st.button("Take me there 🥊"):
+        st.switch_page("pages/Make_Your_Portfolio.py")
+
